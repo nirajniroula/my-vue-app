3bfd3b7cb8585625f885a61cde0e4e6531300c7c
N N <niniroula@gmail.com>
2022-02-24 21:54:20 +0545

    update deploy script

--- scripts/gh-pages-deploy.ts
+++ scripts/gh-pages-deploy.ts
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-console */
-const execa = require('execa')
-const fs = require('fs')
-
+import { execa } from 'execa'
+import fs from 'fs'
 ;(async () => {
   try {
     await execa('git', ['checkout', '--orphan', 'gh-pages'])
